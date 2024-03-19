35e21e4439247bb5184164167a86914539573407
Lafoerte91 <lafoerte@gmail.com>
2024-03-19 20:34:14 +0300

    checkboxes

--- js/main.js
+++ js/main.js
@@ -10,6 +10,7 @@ sidebarToggleBtn.onclick = function() {
 
 // Показать еще 3 карточки
 const btnShowMoreCards = document.querySelector('.btn-more')
+
 btnShowMoreCards.addEventListener('click', function() {
   const hiddenCards = document.querySelectorAll('.card-link--hidden')
   hiddenCards.forEach(item => item.classList.remove('card-link--hidden'))
@@ -17,3 +18,43 @@ btnShowMoreCards.addEventListener('click', function() {
 
 // Показать/скрыть контент внутри виджетов
 const widgets = document.querySelectorAll('.widget')
+
+widgets.forEach(widget => {
+  widget.addEventListener('click', function(evt) {
+    if(evt.target.classList.contains('widget__title')) {
+      evt.target.classList.toggle('widget__title--active')
+      evt.target.nextElementSibling.classList.toggle('widget__body--hidden')
+    }
+  })
+});
+
+/* клие по кнопке Любая и отключение других чекбоксов*/
+const checkBoxAny = document.querySelector('#location-05')
+const topLocationCheckboxes = document.querySelectorAll('[data-location-param]')
+
+checkBoxAny.addEventListener('change', function() {
+  if(this.checked) {
+    topLocationCheckboxes.forEach(item => {
+      item.checked = false
+    });
+  } 
+})
+
+topLocationCheckboxes.forEach(item => {
+  item.addEventListener('change', function() {
+    if(checkBoxAny.checked) {
+      checkBoxAny.checked = false
+    }
+  })
+})
+
+/* Показать еще 3 доп опции с чекбоксами */
+const showMoreOptions = document.querySelector('.widget__show-hidden')
+const hiddenChecknboxes = document.querySelectorAll('.checkbox--hidden')
+
+showMoreOptions.onclick = function() {
+  hiddenChecknboxes.forEach(item => {
+    item.classList.remove('.checkbox--hidden')
+  });
+  this.remove()
+}
